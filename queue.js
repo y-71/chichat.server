809a73c994ae1c84363959839f453d445a35a422
firashmedi <hmedi.firas.hf@gmail.com>
2021-07-25 15:58:35 +0100

    Refactoring code in disconnect & handlePeer

--- queue.js
+++ queue.js
@@ -19,19 +19,11 @@ class Queue {
             return NaN;
         return this.users.shift();
     }
-    // TODO : test this! could be buggy
     delete(socketID){
-        // get the socket index:
-        for (let index = 0 ; index < this.users.length ; i++) {
-            if (socketID === this.users[index].socket.id){
-                console.log('before delete users', this.users);
-                this.users = _.filter(this.users, (user) => {
-                    return user.socket.id !== socketID;
-                });
-                console.log('after delete users', this.users);
-                break;
-            }
-        }
+        // remove correspondant user from provided socket
+        this.users = _.filter(this.users, (user) => {
+            return user.socket.id !== socketID;
+        });
     }
 }
 
